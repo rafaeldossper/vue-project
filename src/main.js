173123b1b76abb779b1128c23a3df9772c613dd3
rafaeldossper <rafahsanttos96@gmail.com>
2022-11-21 21:54:38 +0000

    Atualização do projeto

--- src/main.js
+++ src/main.js
@@ -2,6 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios";
+
+axios.interceptors.request.use(config => {
+  if(store.state.token) {
+    config.headers.Authorization = store.state.token
+  }
+  return config
+})
+axios.interceptors.response.use(res => {
+    return res
+  }, error => {
+    if(error.response.status === 403) {
+      alert('Não autorizado!')
+    }
+    else if (error.response.status === 401) {
+      store.commit('logout')
+      router.push('/login')
+    }
+    throw error
+})
+
+axios.defaults.baseURL =
+  "https://8080-mineda-springexemplo3-405p3beh8sv.ws-us77.gitpod.io/frases";
 
 Vue.config.productionTip = false
 
@@ -9,4 +32,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
